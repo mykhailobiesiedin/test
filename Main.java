d31fffa0f07088cec57b2335f4a1b94474d5491b
mykhailobiesiedin <122889486+mykhailobiesiedin@users.noreply.github.com>
2023-02-17 20:27:09 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,25 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
+
+    static void printResult(String text) {
+        System.out.println(text);
+    }
+
     public static void main(String[] args) {
+        Scanner cleverHouse = new Scanner(System.in);
 
+        printResult("Введите время в часах от 0 до 23 : ");
+        int time = cleverHouse.nextInt();
 
-        Scanner text = new Scanner(System.in);
-        System.out.println("Enter the text : ");
-        String a = text.nextLine();
+        printResult("Введите день недели : ");
+        String day = cleverHouse.next();
 
-        String substring = a.substring(0, 21);
+        printResult("Укажите включена ли сигнализация : ");
+        boolean onOff = cleverHouse.nextBoolean();
 
-        System.out.println("Your text to upper case : " + a.toUpperCase());
+        if ((time == 8) && (!onOff) && (!day.equalsIgnoreCase("Суббота")
+                && (!day.equalsIgnoreCase("Воскресенье")))) {
+            printResult("Поднимаю шторы в 8 часов!");
 
-        System.out.println("Your string length : " + a.length());
+        } else if (((time == 11) && (!onOff)) &&
+                ((day.equalsIgnoreCase("Суббота") || (day.equalsIgnoreCase("Воскресенье"))))) {
+            printResult("Поднимаю шторы в 11 часов!");
 
-        System.out.println("Your first 20 symbols : " + substring + "...");
-
-
-
-
-
-
+        } else if ((time == 20) && (onOff) && (!day.equalsIgnoreCase("Суббота")
+                && (!day.equalsIgnoreCase("Воскресенье")))) {
+            printResult("Включаю чайник!");
+        } else {
+            printResult("Функция с этими данными не предусмотрена");
+        }
     }
 }
