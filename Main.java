c9fd740d6756c9b1f0d8a04c4ff9623bf727210b
mykhailobiesiedin <122889486+mykhailobiesiedin@users.noreply.github.com>
2023-02-27 11:40:19 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,36 +1,46 @@
 import java.util.Scanner;
 
 public class Main {
-
-    static void printResult(String text) {
-        System.out.println(text);
-    }
-
     public static void main(String[] args) {
-        Scanner cleverHouse = new Scanner(System.in);
 
-        printResult("Введите время в часах от 0 до 23 : ");
-        int time = cleverHouse.nextInt();
-
-        printResult("Введите день недели : ");
-        String day = cleverHouse.next();
-
-        printResult("Укажите включена ли сигнализация : ");
-        boolean onOff = cleverHouse.nextBoolean();
-
-        if ((time == 8) && (!onOff) && (!day.equalsIgnoreCase("Суббота")
-                && (!day.equalsIgnoreCase("Воскресенье")))) {
-            printResult("Поднимаю шторы в 8 часов!");
-
-        } else if (((time == 11) && (!onOff)) &&
-                ((day.equalsIgnoreCase("Суббота") || (day.equalsIgnoreCase("Воскресенье"))))) {
-            printResult("Поднимаю шторы в 11 часов!");
-
-        } else if ((time == 20) && (onOff) && (!day.equalsIgnoreCase("Суббота")
-                && (!day.equalsIgnoreCase("Воскресенье")))) {
-            printResult("Включаю чайник!");
-        } else {
-            printResult("Функция с этими данными не предусмотрена");
-        }
+        double number1, number2, result;
+        String operator;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Choose math operator: '+', '-', '*', '/'  or '=' to finish");
+            operator = scanner.next();
+            switch (operator) {
+                case "+" -> {
+                    System.out.println("Enter two numbers : ");
+                    number1 = scanner.nextDouble();
+                    number2 = scanner.nextDouble();
+                    result = number1 + number2;
+                    System.out.println("Result: " + result);
+                }
+                case "-" -> {
+                    System.out.println("Enter two numbers : ");
+                    number1 = scanner.nextDouble();
+                    number2 = scanner.nextDouble();
+                    result = number1 - number2;
+                    System.out.println("Result: " + result);
+                }
+                case "*" -> {
+                    System.out.println("Enter two numbers : ");
+                    number1 = scanner.nextDouble();
+                    number2 = scanner.nextDouble();
+                    result = number1 * number2;
+                    System.out.println("Result: " + result);
+                }
+                case "/" -> {
+                    System.out.println("Enter two numbers : ");
+                    number1 = scanner.nextDouble();
+                    number2 = scanner.nextDouble();
+                    result = number1 / number2;
+                    System.out.println("Result: " + result);
+                }
+                case "=" -> System.exit(0);
+                default -> System.out.println("You entered the wrong operator. Enter the valid one.");
+            }
+        } while (!operator.equals("="));
     }
 }
